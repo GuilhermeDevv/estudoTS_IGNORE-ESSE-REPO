c6106a63d3336eacebbb6f3644f2ed815f49e0ec
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-18 18:05:18 -0300

    outra sintaxe de definir dados

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // 1 - arrays
 var numero = [1, 2, 3];
 var nome = ["Guilherme", "Santos"];
+// 1.1 - array new sintaxe
+var arrayNumberNewSintaxe = [1, 2, 3];
+var arrayStringNewSintaxe = ["Guilherme", "santos"];
--- script.ts
+++ script.ts
@@ -1,4 +1,7 @@
 // 1 - arrays
 const numero: number[] = [1, 2, 3];
-
 const nome: string[] = ["Guilherme", "Santos"];
+// 1.1 - array new sintaxe
+const arrayNumberNewSintaxe: Array<number> = [1, 2, 3];
+const arrayStringNewSintaxe: Array<string> = ["Guilherme", "santos"];
+
