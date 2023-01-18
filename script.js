77e91e7fe6d8e001b03371a54bf0306c0d91e272
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-18 18:28:32 -0300

    função e seu retorno

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ var nome = ["Guilherme", "Santos"];
 // 1.1 - array new sintaxe
 var arrayNumberNewSintaxe = [1, 2, 3];
 var arrayStringNewSintaxe = ["Guilherme", "santos"];
-//2 - any (não usar essa bagaça!!!!!)
-var arr = [1, 2, 3, "string aqui"];
+// 2 - any (não usar essa bagaça!!!!!)
 /*
-o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
-jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
-NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
+            o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
+            jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
+            NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
 */
+var arr = [1, 2, 3, "string aqui"];
+// 3 - funções
+function soma(a, b) {
+    return a + b;
+}
+// 3.1 funções - setando o tipo do retorno da função
+function subtracao(a, b) {
+    return a - b;
+}
--- script.ts
+++ script.ts
@@ -5,13 +5,25 @@ const nome: string[] = ["Guilherme", "Santos"];
 const arrayNumberNewSintaxe: Array<number> = [1, 2, 3];
 const arrayStringNewSintaxe: Array<string> = ["Guilherme", "santos"];
 
-//2 - any (não usar essa bagaça!!!!!)
-
-const arr: Array<any> = [1, 2, 3, "string aqui"];
+// 2 - any (não usar essa bagaça!!!!!)
 
 /*
-o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
-jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
-NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
+            o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
+            jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
+            NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
 */
+const arr: Array<any> = [1, 2, 3, "string aqui"];
+
+// 3 - funções
+
+function soma(a: number, b: number) {
+  return a + b;
+}
+
+// 3.1 funções - setando o tipo do retorno da função
+
+function subtracao(a: number, b: number): number {
+  return a - b;
+}
+
 
