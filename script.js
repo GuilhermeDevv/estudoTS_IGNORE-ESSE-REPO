48d6b14c30dcee84de5b123a62c48df363158b9e
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-18 18:21:09 -0300

    NÃO USE O ANY!

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var nome = ["Guilherme", "Santos"];
 // 1.1 - array new sintaxe
 var arrayNumberNewSintaxe = [1, 2, 3];
 var arrayStringNewSintaxe = ["Guilherme", "santos"];
+//2 - any (não usar essa bagaça!!!!!)
+var arr = [1, 2, 3, "string aqui"];
+/*
+o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
+jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
+NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
+*/
--- script.ts
+++ script.ts
@@ -5,3 +5,13 @@ const nome: string[] = ["Guilherme", "Santos"];
 const arrayNumberNewSintaxe: Array<number> = [1, 2, 3];
 const arrayStringNewSintaxe: Array<string> = ["Guilherme", "santos"];
 
+//2 - any (não usar essa bagaça!!!!!)
+
+const arr: Array<any> = [1, 2, 3, "string aqui"];
+
+/*
+o any aceita qual quer tipo de valor, se for usar o any pra typescript?????????
+jamais,use,o, any,evite! (respeite as virgulas na leitura, percebera que foi em um tom de raiva!)
+NÃO USE O ANY!!!!!!!!!!!!!!!!!!!!
+*/
+
