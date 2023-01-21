782b3ae845c6b752be7ad2f59f36dda39a3d81d8
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-20 22:05:29 -0300

    rascunho

--- RASCUNHO/script.js
+++ RASCUNHO/script.js
@@ -1,3 +1,13 @@
 function soma(a, b) {
     return a + b;
 }
+class pessoa {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    comprimentar(nome, idade) {
+        return `Olá ${nome}, você tem ${idade} anos.`;
+    }
+}
+const pessoa1 = new pessoa("Guilherme", 17);
--- RASCUNHO/script.ts
+++ RASCUNHO/script.ts
@@ -2,3 +2,17 @@ function soma(a: number, b: number): number {
   return a + b;
 }
 
+class pessoa {
+  nome: string;
+  idade: number;
+  constructor(nome: string, idade: number) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+  comprimentar(nome: string, idade: number): string {
+    return `Olá ${nome}, você tem ${idade} anos.`;
+  }
+}
+
+const pessoa1 = new pessoa("Guilherme", 17);
+
