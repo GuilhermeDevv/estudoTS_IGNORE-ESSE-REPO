f36ccba4dde53b25485edecb534164d0afbe339c
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-25 01:16:09 -0300

    null & undefined

--- src/01/t007.ts
+++ src/01/t007.ts
@@ -1,1 +1,12 @@
-//null 7 undefined
+//null & undefined
+let x;
+if (typeof x === 'undefined') x = 10;
+console.log(x * 2);
+
+function quadrado(x: any) {
+  if (typeof x == 'number') return x * x;
+  return null;
+}
+
+quadrado(3);
+quadrado('3');
