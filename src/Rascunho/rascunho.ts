789ec16d466cdfc001354b61ff417c3a4fe732b9
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-27 15:43:58 -0300

    rascunho

--- src/Rascunho/rascunho.ts
+++ src/Rascunho/rascunho.ts
@@ -9,5 +9,17 @@ function organizedNumbers(numbers: number[]): number[] {
   const organizadedNumbers = numbers.sort((a, b) => a - b);
   return organizadedNumbers;
 }
-const result = generate_random_number(100);
+const result = generate_random_number(1);
 console.log(organizedNumbers(result));
+
+const arrString = ['Guilherme', 'Santos'];
+
+function testeRasc(x: string[] | string) {
+  if (Array.isArray(x)) {
+    console.log(`Olá ${x.join(' ')}`);
+  } else {
+    console.log(x);
+  }
+}
+
+testeRasc(arrString);
