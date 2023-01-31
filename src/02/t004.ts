3b9542f2a28b2ce3b92887fbf51e1e1259b48e97
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-01-31 02:06:19 -0300

    generics 01

--- src/02/t004.ts
+++ src/02/t004.ts
@@ -1,5 +1,6 @@
 type paramsCallbackFn<U> = (value: U, index?: number, array?: U[]) => boolean;
-function ...<T>(array: T[], callbackfn: paramsCallbackFn<T>): T[] {
+
+function filter<T>(array: T[], callbackfn: paramsCallbackFn<T>): T[] {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
     if (callbackfn(array[i])) newArray.push(array[i]);
@@ -7,4 +8,4 @@ function ...<T>(array: T[], callbackfn: paramsCallbackFn<T>): T[] {
   return newArray;
 }
 const array = [1, 2, 3, 4];
-filter(array, (value) => value <= 3));
+filter(array, (value) => value <= 3);
