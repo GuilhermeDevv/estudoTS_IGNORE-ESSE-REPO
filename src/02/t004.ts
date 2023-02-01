1d9cdc9e437c7933f521c45359330ebf6e672b41
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-01 14:51:37 -0300

    valor inferido automaticamente

--- src/02/t004.ts
+++ src/02/t004.ts
@@ -1,6 +1,6 @@
-type paramsCallbackFn<U> = (value: U, index?: number, array?: U[]) => boolean;
+type paramsCallbackFn = <U>(value: U, index?: number, array?: U[]) => boolean;
 
-function filter<T>(array: T[], callbackfn: paramsCallbackFn<T>): T[] {
+function filter<T>(array: T[], callbackfn: paramsCallbackFn): T[] {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
     if (callbackfn(array[i])) newArray.push(array[i]);
@@ -8,4 +8,5 @@ function filter<T>(array: T[], callbackfn: paramsCallbackFn<T>): T[] {
   return newArray;
 }
 const array = [1, 2, 3, 4];
-filter(array, (value) => value <= 3);
+
+console.log(filter(array, (value) => value > 2));
